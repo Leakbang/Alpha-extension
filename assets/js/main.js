ee0503a11dd072fde68f2de41e18b460c6d460c6
Leakbang <51197897+Leakbang@users.noreply.github.com>
2021-03-23 21:08:28 +0100

    0.5
    - Added dark mode function
    -  Fixed clicking on the version text does not take the user to the Github repo

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,8 @@ document.getElementById("close-menu").addEventListener("click", QuitSettings);
 document.getElementById("load").addEventListener("click", Load);
 document.getElementById("save").addEventListener("click", Save);
 document.getElementById("AutoSaveEnabled").addEventListener("click", SaveAutoSaveState);
+document.getElementById("DarkModeEnabled").addEventListener("click", ToggleDarkMode);
+document.getElementById("version-text").addEventListener("click", OpenAuthorWebsite);
 
 document.getElementById("search").addEventListener("click", SearchQuery);
 document.getElementById("search-form").addEventListener("submit", SearchQuery);
@@ -18,6 +20,18 @@ function SearchQuery(event) {
     event.preventDefault();
     var SQuery = "https://duckduckgo.com/?q=" + document.getElementById("search-box").value
     window.location.href = SQuery
+}
+
+function OpenAuthorWebsite () {
+    window.location.href = "https://github.com/Leakbang/Alpha-extension"
+}
+
+function ToggleDarkMode () {
+    document.body.classList.toggle("inversion")
+    document.getElementById("dark-background").classList.toggle("display-inversion")
+    
+    var IsDarkModeEnabled = document.getElementById("DarkModeEnabled").checked
+    localStorage.setItem("DarkMode", IsDarkModeEnabled)
 }
 
 function OpenSearchMenu () {
@@ -82,6 +96,14 @@ function InitializeSettings() {
         localStorage.setItem("AutoSave", "true");
     }
     document.getElementById("AutoSaveEnabled").checked = localStorage.getItem("AutoSave") == "true";
+    if (localStorage.getItem("DarkMode") === null) {
+        localStorage.setItem("DarkMode", "false");
+    }
+    document.getElementById("DarkModeEnabled").checked = localStorage.getItem("DarkMode") == "true";
+    
+    if (localStorage.getItem("DarkMode") == "true") {
+        ToggleDarkMode();
+    }
 
     document.getElementById("version-text").innerHTML = document.getElementById("version-text").innerHTML + browser.runtime.getManifest().version
 }
